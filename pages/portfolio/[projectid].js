3cf5aa6d99241d67be2b71ea55d31d94488ee83b
Ravi Ranjan <2140131@sliet.ac.in>
2024-01-11 23:01:22 +0530

    adding useRouter functionalities in dynamic page

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,13 @@
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage(){
+    const router = useRouter();
+
+    // console.log(router.pathname);
+    // console.log(router.query);
+
+    //we can get the projectid by infering router.query.projectid
+
     return (
         <h1>The PortfolioProjectPage</h1>
     )
